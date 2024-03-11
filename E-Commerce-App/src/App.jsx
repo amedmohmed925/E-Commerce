61fdb8bf95026985bc8b288034f76d0f461f8843
Ahmed Mohamed <118309402+amedmohmed925@users.noreply.github.com>
2024-03-11 21:51:49 +0200

    section Four

--- E-Commerce-App/src/App.jsx
+++ E-Commerce-App/src/App.jsx
@@ -2,12 +2,13 @@
 import './App.css'
 import SectionThree from './components/sectionThree'
 import './index.css'
-
+import Combonet from './components/Combonet'
 function App() {
 
   return (
     <div>
         <SectionThree />
+        <Combonet />
     </div>
   )
 }
--- E-Commerce-App/src/components/combonet.jsx
+++ E-Commerce-App/src/components/combonet.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './SummerSale.css'; 
+import '../SummerSale.css'; 
 import CardsSectionThree from './Cards'; 
 
 class SummerSale extends Component {
@@ -31,33 +31,31 @@ class SummerSale extends Component {
   }
 
   render() {
-    const { days, hours, minutes, seconds } = this.state;
     return (
       <div className='bg'>
+<div className="pannar pannar-2 text-bg-success p-5  ">
+                <div className="textOne ">
+                    <span className='fs-6'>20% OFF</span>
+                    <h1 className='display-2 fw-bold '>HAPP <br /> HOURS</h1>
+                    <span className='fs-6'>15 New To 7 Dec</span>
+                </div>
 
-        <div className="summer-sale-container">
-          <div className="left-content">
-            <h1>Summer Sale HAPP HOUR</h1>
-          </div>
-          <div className="middle-content">
-            <h2>Special Offer!</h2>
-            <img src="./WhatsApp Image 2024-03-10 at 15.56.25_8dca38fd.jpg" alt="Special Offer" className="special-offer-image" />
-            <p>Get 50% off on all products</p>
-          </div>
-          <br></br>
-          <div className="right-content">
-            <div className="countdown">
-              <span>{days}</span>
-              <span>{hours}</span>
-              <span>{minutes}</span>
-              <span>{seconds}</span>
+                <img className='w-50 p-3 d-none d-sm-block img-fluid img-headPhone' src="/images/wirliss.png" alt="headPhone" />
+
+                <div className="textTwo ">
+                    <span className='fs-6'>Beats Solo Air</span>
+                    <h1 className='display-5 fw-bold'>Summer Sale</h1>
+                    <p className='fs-6'>Company that’s grown from 270 to 480 employees <br /> in the last 12 months.</p>
+                    <button className='btn btn-light rounded-pill px-4'>Shop</button>
+                </div>
+
             </div>
-            <button>Shop Now</button>
-          </div>
-        </div>
+            <div className="cards py-5 d-flex flex-column justify-content-center">
+                <h1 className='mx-auto fw-bold'>Recent News</h1>
+                <p className='mx-auto '>There are many variations passages</p>
+              
+            </div>
 
-        <CardsSectionThree /> 
-        
       </div>
     );
   }
