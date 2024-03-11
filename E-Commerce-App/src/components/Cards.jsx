52b787cc2af7e02a22440c07135256c57ec4cff8
yasmineaboelqassem <159438555+yasmineaboelqassem@users.noreply.github.com>
2024-03-11 20:53:43 +0200

    Create Cards.jsx
    Signed-off-by: yasmineaboelqassem <159438555+yasmineaboelqassem@users.noreply.github.com>

---
+++ E-Commerce-App/src/components/Cards.jsx
@@ -0,0 +1,30 @@
+// CardsSectionThree.js
+import React from 'react';
+
+function Cards() {
+    const arr = [
+        { title: "hour", src: '/close-up-hand-using-smartwatch.jpg', para: "$1040", },
+        { title: "laptop", src: './hhhhe.jpg', para: "$300", },
+        { title: "Virtual reality", src: '/portrait-young-african-american-man-with-vr-glasses.jpg', para: "$8000", },
+        
+
+    ]
+
+    return (
+        <div className='main py-5 d-flex w-100 flex-wrap gap-3'>
+            {arr.length >= 1 ? (
+                arr.map((e, i) => (
+                    <div className='card-box box mx-auto' key={i}>
+                        <img className='w-100 rounded' src={e.src} alt={e.title} />
+                        <h2 className='fs-4 fw-light'>{e.title}</h2>
+                        <p className='fw-bold'>{e.para}</p>
+                    </div>
+                ))
+            ) : (
+                <h1 className='error'>Not Found</h1>
+            )}
+        </div>
+    );
+}
+
+export default Cards;
