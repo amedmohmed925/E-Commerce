ef77f80980896506719b620f39f5964b4bab99b6
yasmineaboelqassem <159438555+yasmineaboelqassem@users.noreply.github.com>
2024-04-06 02:09:50 +0200

    Update Login.jsx
    Signed-off-by: yasmineaboelqassem <159438555+yasmineaboelqassem@users.noreply.github.com>

--- E-Commerce-App/src/components/LoginPage/Login.jsx
+++ E-Commerce-App/src/components/LoginPage/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'; 
 import axios from 'axios';
-import './UserLoginForm.css';
+import './Login.css';
 
 const UserLoginForm = () => {
   const history = useHistory(); 
